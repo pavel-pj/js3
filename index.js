c778498c41176ce83fb837480eccd6d2ab42c6c6
Pavel <admin@pavel.ru>
2025-01-30 09:00:38 +0300

    promise

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-HELLOOO
+//import { touch } from './src/async-7.js';
+import fsp from 'fs/promises';
+import fs from 'fs';
+
+
+ const touch = (path) =>
+{
+   return fsp.access(path)
+       .catch(()=> fsp.writeFile(path, ''))
+
+}
+
+
+touch('file.js');//.then(() => console.log('created'));
+ 
---
+++ src/async-7.js
@@ -0,0 +1,10 @@
+import fsp from 'fs/promises';
+
+export const touch = () =>
+{
+    console.log("Hello");
+}
+
+export default touch
+
+//touch('/myfile').then(() => console.log('created!'));
