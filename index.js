cadbb47e35d24500cc12f1eec70ace695bb86e66
pavel-pj <voroshilow_pavel@mail.ru>
2025-01-31 22:31:17 +0300

    function 9

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-import { exchange } from './src/async-14.js';
+import { takeOldest  } from './src/func-9.js';
 import fsp from 'fs/promises';
 import _ from 'lodash';
 
-exchange('src/folder/top1.js', 'src/folder/top2.js');
-//console.log("sefsef")
+const users = [
+    { name: 'Tirion', birthday: 'Nov 19, 1988' },
+    { name: 'Sam', birthday: 'Nov 22, 1999' },
+    { name: 'Rob', birthday: 'Jan 11, 1975' },
+    { name: 'Sansa', birthday: 'Mar 20, 2001' },
+    { name: 'Tisha', birthday: 'Feb 27, 1992' },
+    { name: 'Chris', birthday: 'Dec 25, 1995' },
+];
+
+
+
+console.log(takeOldest(users));
+
---
+++ src/func-9.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+export const takeOldest = (users, num = 1) =>{
+
+    const now = Date.now();
+
+    const newUsers = users.sort((a,b)=>{
+
+        const ageA =now - Date.parse(a.birthday);
+        const ageB =now - Date.parse(b.birthday);
+
+        return (ageA < ageB) ? 1 : -1;
+
+    })
+    return _.take(newUsers, num );
+
+}
+
+export default takeOldest;
