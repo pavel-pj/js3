592cb8f493ce32b9483d4671ac1696cad2febcaa
pavel-pj <voroshilow_pavel@mail.ru>
2025-02-03 18:33:54 +0300

    p2

--- index.js
+++ index.js
@@ -1,29 +1,89 @@
 import fsp from 'fs/promises';
 import _ from 'lodash';
-import makeUser from './src/oop/les-1.js';
-import { Point,getX,getY } from './src/oop/Point.js';
-import { Segment, getBeginPoint,getEndPoint} from './src/oop/Segment.js';
-import {reverse} from './src/oop/solution.js'
 
-let beginPoint = new Point(1, 10);
-let endPoint = new Point(11, -3);
-
-const segment = new Segment(beginPoint, endPoint);
+import { Money as Money } from './src/oop/prototype.js';
 
 
-const reversedSegment = reverse(segment);
+const money1 = new Money(100);
+console.log(money1.exchangeTo('eur').getValue());
+//console.log(money1.exchangeTo('eur').getCurrency());
+//console.log(money1.getValue());
+//console.log(money1.getCurrency());
 
-// прямое обращение к свойствам приведено только в демонстрационных целях
-console.log(
-    segment.beginPoint.x,
-    segment.beginPoint.y,
-    segment.endPoint.x,
-    segment.endPoint.y,
-);// => 1 10 11 -3
+console.log(money1.exchangeTo('usd').getValue());
+console.log(money1.getValue());
+console.log(money1.getCurrency());
 
-console.log(
-    reversedSegment.beginPoint.x,
-    reversedSegment.beginPoint.y,
-    reversedSegment.endPoint.x,
-    reversedSegment.endPoint.y,
-); // => 11 -3 1 10
+const money2 = new Money(200, 'eur');
+const money3 = money2.add(money1)
+console.log(money3.getValue());
+console.log(money2.getValue());
+console.log(money3.getCurrency());
+
+
+
+
+//const money2 = new Money(200, 'eur');
+
+//const money3 = money2.add(money1);
+
+
+
+//const money4 = money1.add(money2);
+//console.log(money4.getValue())
+
+/*
+const money1 = new Money(100);
+
+// Возвращает значение
+console.log(money1.getValue()); // 100
+console.log(money1.getCurrency()); // 'usd'
+console.log(money1.format())
+// Конвертирует в указанную валюту и возвращает новое значение
+console.log(money1.exchangeTo('eur').getValue()); // 70
+console.log(money1.exchangeTo('eur').getValue()); // 70
+console.log(money1.getCurrency()); // eur
+console.log(money1.exchangeTo('usd').getValue()); // 70
+
+
+
+/*
+const money2 = new Money(200, 'eur');
+console.log(money2.getValue()); // 200
+const money3 = money2.add(money1);
+console.log( money3.getValue()) //270
+console.log(money3.format())
+const money4 = money3.add(money1);
+console.log(money4.getValue()); // 340
+
+const money5 = new Money(10000);
+console.log(money5.format()); // "$10,000.00"
+
+console.log(money4.format())
+*/
+
+
+
+
+/*
+const numbers1 = [1, 3];
+
+
+Array.prototype.last = function last(){
+    return this[this.length - 1];
+}
+
+console.log(numbers1.last())
+
+
+/*
+
+// Как только выполнится этот код, все массивы,
+// включая уже созданные, обзаведутся методом last
+Array.prototype.last = function last() {
+    // Такое обращение сработает, ведь this — это ссылка на сам объект,
+    // который в нашем случае является массивом
+    return this[this.length - 1];
+}
+
+console.log(numbers1.last()); // 3*/
---
+++ src/oop/prototype.js
@@ -0,0 +1,56 @@
+export function Money (value, currency ='usd' ) {
+    this.value = value;
+    this.currency = currency;
+
+}
+
+Money.prototype.getValue = function getValue() {
+    return this.value;
+}
+
+Money.prototype.getCurrency  = function getCurrency() {
+    return this.currency;
+}
+
+Money.prototype.exchangeTo = function exchangeTo(newCur) {
+    if (this.currency === newCur) return this;
+
+    if (this.currency ==='usd') {
+        this.currency = 'eur';
+        this.value *= 0.7;
+        return this;
+    }
+    if (this.currency ==='eur') {
+        this.currency = 'usd'
+        this.value =this.value * 1.2
+        return this;
+    }
+
+}
+
+Money.prototype.add = function add(money) {
+    const curr = money.getCurrency();
+
+
+    if (this.getCurrency() === money.getCurrency() ) {
+        return new Money (this.getValue() + money.getValue() , curr);
+    }
+
+    else if (this.getCurrency !== curr ){
+        if (curr === 'usd'){
+            money.exchangeTo('eur')
+        }
+        else if ( curr === 'eur') {
+            money.exchangeTo('usd')
+        }
+    }
+    return new Money (this.getValue() + money.getValue() , curr);
+
+}
+
+Money.prototype.format = function format() {
+    return this.getValue().toLocaleString('en-US', { style: 'currency', currency: this.getCurrency() });
+}
+
+
+export default { Money }
