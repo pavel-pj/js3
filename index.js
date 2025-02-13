fe112711f4d7237b3033ff2aaafbac7959ef8746
pavel-pj <voroshilow_pavel@mail.ru>
2025-02-13 21:55:00 +0300

    accum tree

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { du,countFiles} from './src/tree/sizes.js'
+import { acc} from './src/tree/les-6.js'
 import _ from 'lodash'
 import * as fsTrees from '@hexlet/immutable-fs-trees';
 
@@ -6,18 +6,18 @@ const tree = fsTrees.mkdir('/', [
     fsTrees.mkdir('etc', [
         fsTrees.mkdir('apache'),
         fsTrees.mkdir('nginx', [
-            fsTrees.mkfile('nginx.conf', { size: 800 }),
+            fsTrees.mkfile('nginx.conf'),
         ]),
         fsTrees.mkdir('consul', [
-            fsTrees.mkfile('config.json', { size: 1200 }),
-            fsTrees.mkfile('data', { size: 8200 }),
-            fsTrees.mkfile('raft', { size: 80 }),
+            fsTrees.mkfile('config.json'),
+            fsTrees.mkdir('data'),
         ]),
     ]),
-    fsTrees.mkfile('hosts', { size: 3500 }),
-    fsTrees.mkfile('resolve', { size: 1000 }),
+    fsTrees.mkdir('logs'),
+    fsTrees.mkfile('hosts'),
 ]);
 
+console.log(acc(tree,2));
 
-console.log(du(    fsTrees.getChildren(tree)[0]         )    )  
+
 
